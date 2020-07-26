cff1e5279bd2d8fe1eeba1546cbbb0ef003002a6
Md Shoaib <shoaib10oct@gmail.com>
2020-07-27 01:01:46 +0530

    SeekBar Implementation

---
+++ 12SeekBarImplementation/app/src/main/java/com/example/a12seekbarimplementation/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.a12seekbarimplementation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //variable declaration
+    SeekBar seekBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Assign ui elements
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+
+        //Listeners
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setTextSize(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
