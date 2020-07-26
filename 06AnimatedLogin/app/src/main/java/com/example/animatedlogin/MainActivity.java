be8edfc98710361b1e9745764f1899e79853d324
Md Shoaib <shoaib10oct@gmail.com>
2020-07-26 12:34:15 +0530

    Animation And custom button

---
+++ 06AnimatedLogin/app/src/main/java/com/example/animatedlogin/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.animatedlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.animation.Animation;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Variable declaration
+    private ConstraintLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Assign ui elements
+        layout = findViewById(R.id.layout);
+
+        //Setting animation
+        AnimationDrawable animationDrawable = (AnimationDrawable) layout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
+    }
+}
